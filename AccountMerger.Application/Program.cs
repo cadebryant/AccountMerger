ed421fdc5a620243a58da86ad524a454639042a1
Cade Bryant <cadehbryant@gmail.com>
2022-10-16 02:21:27 -0400

    added console app cpde

--- AccountMerger.Application/Program.cs
+++ AccountMerger.Application/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AccountMerger.Library;
+using AccountMerger.Library.Interface;
+using AccountMerger.Library.Merger;
+using AccountMerger.Library.Merger.Impl;
+using AccountMerger.Library.Merger.Interface;
+using AccountMerger.Library.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+IObjectMerger<IAccount, Person> merger = new AccountPersonMerger();
+var jsonSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+var acctsPath = "Data/accounts.json";
+var accountsJson = await File.ReadAllTextAsync(acctsPath);
+var accounts = JsonConvert.DeserializeObject<IEnumerable<Account>>(accountsJson, jsonSettings);
+if (accounts == null || !accounts.Any())
+{
+    Console.WriteLine("Accounts could not be parsed from JSON.");
+    return;
+}
+var results = merger.Merge(accounts);
+if (results == null || !results.Any())
+{
+    Console.WriteLine("Accounts could not be merged.");
+    return;
+}
+Console.WriteLine(JsonConvert.SerializeObject(results, jsonSettings));
